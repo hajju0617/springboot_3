bf7a05864527ce718c16f7cff677b673d3334daa
hajju0617 <joowon023617@gmail.com>
2024-08-12 01:17:15 +0900

    Springboot_3 - test

--- src/main/java/org/example/springboot_3/controller/FirstController.java
+++ src/main/java/org/example/springboot_3/controller/FirstController.java
@@ -9,7 +9,7 @@ public class FirstController {
 
     @GetMapping("/hi")
     public String niceToMeetYou(Model model) {  // model 객체 받아오기
-        model.addAttribute("username", "하쮸");  // model 객체가 "하주원"값을 "username"에 연결해 웹 브라우저로 보냄.
+        model.addAttribute("username", "하주원");  // model 객체가 "하주원"값을 "username"에 연결해 웹 브라우저로 보냄.
         return "greetings";     // greetings.mustache 파일 반환
     }
 }
