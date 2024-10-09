d087559431a2c41f73483a96ba389199530a474a
hajju0617 <joowon023617@gmail.com>
2024-10-10 02:32:53 +0900

    Springboot_3 - Comment

--- src/main/java/org/example/springboot_3/repository/CommentRepository.java
+++ src/main/java/org/example/springboot_3/repository/CommentRepository.java
@@ -7,7 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    // 모든 댓글 조회
+    // 특정 게시글의 모든 댓글 조회
     @Query(value = "SELECT * FROM comment WHERE article_id = :articleId", nativeQuery = true)
-    List<Comment> findCommentByArticleId(Long articleId);
+    List<Comment> findByArticleId(Long articleId);
+
+    // 특정 닉네임이 쓴 모든 댓글 조회
+    List<Comment> findByNickname(String nickname);
 }
